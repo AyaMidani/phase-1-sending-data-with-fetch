767e6840f2f613558b452c260056aba1e11fb933
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-06-03 01:52:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name,email){
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name:name,
+        email:email,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+      document.body.innerHTML=object.id
+  })
+  .catch(function (error) {
+    alert("Unauthorized Access");
+    document.body.innerHTML="Unauthorized Access"
+    console.log(error.message);
+    });
+}
+submitData("Sam","sam@sam.com")
